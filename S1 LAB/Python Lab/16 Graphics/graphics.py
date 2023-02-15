542e415980246a01b1486d69f9db48e21afbcf40
ananthudev <ananthudevdv@gmail.com>
2023-02-15 21:04:52 +0530

    Added new python files

--- S1 LAB/Python Lab/16 Graphics/graphics.py
+++ S1 LAB/Python Lab/16 Graphics/graphics.py
@@ -1,40 +1,38 @@
-from graphics.rectangle import *
-from graphics.circle import *
-from graphics.tdgraphics.sphere import *
-from graphics.tdgraphics.cuboid import *
+from graphics.rectangle import rectarea, rectperi
+from graphics.circle import circlearea, circleperi
+from graphics.tdgraphics.sphere import spherearea, sphereperi
+from graphics.tdgraphics.cuboid import cuboidarea, cuboidperi
 
 print("Select operation.")
-print("1.Rectangle")
-print("2.Circle")
-print("3.Sphere")
-print("4.Cuboid")
-print("5.Exit")
+print("1. Rectangle")
+print("2. Circle")
+print("3. Sphere")
+print("4. Cuboid")
+print("5. Exit")
 
 while True:
-    choice = input("Enter choice(1/2/3/4/5): ")
+    choice = input("Enter choice (1/2/3/4/5): ")
     if choice in ('1', '2', '3', '4', '5'):
         if choice == '1':
-            l = int(input("Enter the length of rectangle:"))
-            b = int(input("Enter the breadth of rectangle:"))
-            print("Area of rectangle=", rectarea(l, b))
-            print("Perimeter of rectangle=", rectperi(l, b))
+            length = int(input("Enter the length of rectangle: "))
+            breadth = int(input("Enter the breadth of rectangle: "))
+            print("Area of rectangle: ", rectarea(length, breadth))
+            print("Perimeter of rectangle: ", rectperi(length, breadth))
         elif choice == '2':
-            r = int(input("Enter the radius of circle:"))
-            print("Area of circle=", circlearea(r))
-            print("Perimeter of circle=", circleperi(r))
+            radius = int(input("Enter the radius of circle: "))
+            print("Area of circle: ", circlearea(radius))
+            print("Circumference of circle: ", circleperi(radius))
         elif choice == '3':
-            sr = int(input("Enter the radius of sphere:"))
-            print("Area of sphere=", spherearea(sr))
-            print("Perimeter of sphere=", sphereperi(sr))
+            radius = int(input("Enter the radius of sphere: "))
+            print("Surface area of sphere: ", spherearea(radius))
+            print("Volume of sphere: ", sphereperi(radius))
         elif choice == '4':
-            cl = int(input("Enter the length of cuboid:"))
-            w = int(input("Enter the width of cuboid:"))
-            h = int(input("Enter the height of cuboid:"))
-            print("Area of cuboid=", cuboidarea(cl, w, h))
-            print("Perimeter of cuboid=", cuboidperi(cl, w, h))
+            length = int(input("Enter the length of cuboid: "))
+            width = int(input("Enter the width of cuboid: "))
+            height = int(input("Enter the height of cuboid: "))
+            print("Surface area of cuboid: ", cuboidarea(length, width, height))
+            print("Volume of cuboid: ", cuboidperi(length, width, height))
         elif choice == '5':
             exit(0)
-        else:
-            print("Invalid Input")
     else:
-        print("Invalid Input")
+        print("Invalid input. Please enter a valid choice.")  
